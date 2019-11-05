6bb439f9bd7db943820399750ea44322b80ee82e
William Lukas <iggyboy456@gmail.com>
2019-11-05 10:55:57 -0600

    stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
+import Jumbotron from "./components/Jumbotron"
+import Nav from "./components/Navbar";
+import { Input, FormBtn } from "./components/Form"
+import { List, ListItem } from "./components/List"
 class App extends Component {
+  state = {
+    books: [],
+    title: "",
+    author: "",
+    synopsis: ""
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Nav></Nav>
+        <Jumbotron></Jumbotron>
+        <Input></Input>
+        <FormBtn>Submit</FormBtn>
       </div>
     );
   }
